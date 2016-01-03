4c52fe6d61a6a60c85dcb30e151c7359a53245fb
Jerry <quinteroheg2@lopers.unk.edu>
2016-01-03 14:21:50 -0600

    Fixed a small challenge error.

--- src/EasyLevel/Python/ReverseWords.py
+++ src/EasyLevel/Python/ReverseWords.py
@@ -1,8 +1,9 @@
 import sys
 
-theFile = open(sys.argv[1], "r")
-for line in theFile:
-	printReverse(line.strip())
-
 def printReverse(phrase):
 	print(phrase[phrase.find(' ') + 1:len(phrase)], phrase[:phrase.find(' ')])
+
+theFile = open(sys.argv[1], "r")
+for line in theFile:
+	if line.strip() != "":
+		printReverse(line.strip())
